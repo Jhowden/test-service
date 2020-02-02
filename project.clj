7a5f242a9cac7215ccc50706daaf93bff44b342a
Jonathan Howden <jonathanhowden@Jonathans-MacBook-Pro.local>
2020-02-01 18:38:04 -0600

    Remove reagent

--- project.clj
+++ project.clj
@@ -12,8 +12,6 @@
                  ;; [io.pedestal/pedestal.immutant "0.5.7"]
                  ;; [io.pedestal/pedestal.tomcat "0.5.7"]
 
-                 [reagent "0.9.1"]
-
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.26"]
                  [org.slf4j/jcl-over-slf4j "1.7.26"]
