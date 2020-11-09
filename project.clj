260ece73139b2fa29057eafb894c49fca59b4dfb
Jonathan Howden <jonathan.howden@reverb.com>
2020-11-09 13:46:20 -0600

    Add figwheel and reagent

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.pedestal/pedestal.service "0.5.7"]
+                 [io.pedestal/pedestal.service "0.5.8"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.7"]
-                 ;; [io.pedestal/pedestal.immutant "0.5.7"]
-                 ;; [io.pedestal/pedestal.tomcat "0.5.7"]
-                 [hiccup "1.0.5"]
+                 [io.pedestal/pedestal.jetty "0.5.8"]
+                 ;; [io.pedestal/pedestal.immutant "0.5.8"]
+                 ;; [io.pedestal/pedestal.tomcat "0.5.8"]
+                ;  [hiccup "1.0.5"]
+                 [reagent "0.9.1"]
 
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.26"]
@@ -21,7 +22,22 @@
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
   ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
-  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "test-service.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]]}
-             :uberjar {:aot [test-service.server]}}
+  :profiles {
+             :dev {
+                   :aliases {"run-dev" ["trampoline" "run" "-m" "test-service.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.8"]]
+                  }
+             :uberjar {
+                       :aot [test-service.server]
+                      }
+            }
+  :plugins [[lein-figwheel "0.5.13"]]
+  :clean-targets [:target-path "target"]
+  :cljsbuild {
+    :builds [{:id "dev"
+              :source-paths ["src/clj" "src/cljc"]
+              :figwheel true
+              :compiler {:main "test-service.server"}
+             }]
+   }
   :main ^{:skip-aot true} test-service.server)
